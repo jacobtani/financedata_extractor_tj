5da34110a02f077892938cbca2714ed9d1888314
Tania Jacob <tanzjacob@gmail.com>
2016-02-06 12:51:16 +1300

    Retrieve fixed commodities prices from api

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
+require 'net/http'
+
  class ItemsController < ApplicationController
-  before_action :set_item, only: [:edit, :update, :show, :destroy]
   
   def index
     @items = Item.all
@@ -9,50 +10,8 @@
     @item = Item.find(params[:id])
   end
 
-  def new
-    @item = Item.new
+  def retrieve_current_data
+    @quote_data = Item.current_data
   end
-
-  def create
-    @item = Item.new item_params
-    respond_to do |format|
-      if @item.save
-        flash[:success] = "Item was created successfully."
-        format.js { redirect_turbo admin_path}
-      else
-        format.js { render partial: 'shared/ajax_form_errors', locals: {model: @item}, status: 500 }
-      end
-    end
-  end
-
-  def edit
-  end
-
-  def update
-    respond_to do |format|
-      if @item.update_attributes item_params
-        flash[:success] = "Item was updated successfully."
-        format.js {redirect_turbo admin_path}
-      else
-        format.js { render partial: 'shared/ajax_form_errors', locals: {model: @item}, status: 500 }
-      end
-    end
-  end
-
-  def destroy
-    @item.destroy
-    redirect_to admin_path
-  end
-
-  private
-
-    def item_params
-      params.require(:item).permit(:name, :last_trade_date, :last_trade_time, :last_trade_price)
-    end
-
-    def set_item
-      @item = Item.find params[:id] rescue nil
-      return not_found! unless @item
-    end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -5,6 +5,14 @@ class Item
   attribute :last_trade_date
   attribute :last_trade_price
 
-validates_presence_of :name, :last_trade_date, :last_trade_time, :last_trade_price
+  validates_presence_of :name, :last_trade_date, :last_trade_time, :last_trade_price
+
+  def self.current_data
+    url = URI.parse('https://query.yahooapis.com/v1/public/yql?q=select%20Name%2C%20LastTradePriceOnly%2C%20LastTradeDate%2C%20LastTradeWithTime%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22CLH16.NYM%22%2C%20%22BZJ16.NYM%22%2C%20%22HOH16.NYM%22%2C%20%22NGH16.NYM%22%2C%20%22RBH16.NYM%22%2C%20%22HGG16.CMX%22%2C%20%22GCG16.CMX%22%2C%20%22PAG16.NYM%22%2C%20%22PLH16.NYM%22%2C%20%22SIG16.CMX%22%20)&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys')
+    data = Net::HTTP.get_response(url).body
+    xml_data = Hash.from_xml(data)
+    @quote_data = xml_data ["query"]["results"]["quote"]
+    @quote_data
+  end
 
 end
