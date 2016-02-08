5a24cfd9085979f2c6cc1e6a9e92710f581cc51c
Tania Jacob <tanzjacob@gmail.com>
2016-02-08 23:21:26 +1300

    Code cleanup

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,12 +13,10 @@ require 'net/http'
   def retrieve_current_data
     @item = Item.create(data: nil)
     #ItemsWorker.perform_in(1.minutes, @item.id)
-    @blah = ItemsWorker.perform_async(@item.id)
+    ItemsWorker.perform_async(@item.id)
     @item.reload
     sleep(20)
     @item.reload
-    @item.reload
-    logger.info ('item has a data of ' + @item.reload.data)
     @quote_data = @item.reload.data
   end
 
