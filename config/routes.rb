b6650cd48772b4e9a09aed65fc3064aaef027f16
Tania Jacob <tanzjacob@gmail.com>
2016-02-15 22:14:17 +1300

    Remove sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
-require 'sidekiq/web'
-
 Rails.application.routes.draw do
-   mount Sidekiq::Web, at: "/sidekiq"
    root 'items#retrieve_current_data'
    get '/data' => 'items#retrieve_current_data', as: :retrieve_data
    get '/history' => 'items#search_history', as: :search_history
