59f92fe49273d36988000daab888a7e6e8c7d9b5
Tania Jacob <tanzjacob@gmail.com>
2016-02-18 15:45:38 +1300

    Add siteconfig model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160217021614) do
+ActiveRecord::Schema.define(version: 20160217202052) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,11 @@ ActiveRecord::Schema.define(version: 20160217021614) do
     t.float    "last_price"
     t.datetime "created_at",                    null: false
     t.string   "symbol"
+  end
+
+  create_table "site_configs", force: :cascade do |t|
+    t.boolean "running_start"
+    t.boolean "running_stop"
   end
 
 end
