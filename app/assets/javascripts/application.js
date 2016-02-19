21153f0ceede61c208803d645ac8ed5a6cd828ec
Tania Jacob <tanzjacob@gmail.com>
2016-02-20 12:25:58 +1300

    Add flash messages for error handling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap-sprockets
+//= require flash_messages
 //= require message-bus
 //= require custom-message-bus
 //= require_tree .
