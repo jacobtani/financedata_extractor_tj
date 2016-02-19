6c8cbbe98d918d22d5ec6e7cec56f8bd11b193ab
Tania Jacob <tanzjacob@gmail.com>
2016-02-20 00:08:55 +1300

    Make dates consistent on UI

--- app/assets/javascripts/custom-message-bus.js
+++ app/assets/javascripts/custom-message-bus.js
@@ -1,14 +1,24 @@
-// For the time now
-Date.prototype.timeNow = function () {
-     return ((this.getHours() < 10)?"0":"") + this.getHours() +":"+ ((this.getMinutes() < 10)?"0":"") + this.getMinutes() +":"+ ((this.getSeconds() < 10)?"0":"") + this.getSeconds();
-}
-
 var checkCurrentData, initMessageBus,
   indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
 
 initMessageBus = function() {
   MessageBus.start();
   return MessageBus.callbackInterval = 1000;
+};
+
+formatDate = function (currentDate) {
+  var twoDigitMonth=((currentDate.getMonth()+1)>=10)? (currentDate.getMonth()+1) : '0' + (currentDate.getMonth()+1);  
+  var twoDigitDate=((currentDate.getDate())>=10)? (currentDate.getDate()) : '0' + (currentDate.getDate());
+  var date_formatted = twoDigitDate + "-" + twoDigitMonth + "-" + currentDate.getFullYear(); 
+  return date_formatted
+};
+
+formatTime = function (time) {
+  var hours = ((time.getHours())>=10)? (time.getHours()) : '0' + (time.getHours());
+  var minutes = ((time.getMinutes())>=10)? (time.getMinutes()) : '0' + (time.getMinutes());
+  var seconds = ((time.getSeconds())>=10)? (time.getSeconds()) : '0' + (time.getSeconds());
+  var time_formatted = hours + ":" + minutes + ":" + seconds
+  return time_formatted
 };
 
 checkCurrentData = function() {
@@ -26,13 +36,11 @@ checkCurrentData = function() {
       
       //Configure format of date 
       var currentDate = new Date(item.LastTradeDate);
-      var twoDigitMonth=((currentDate.getMonth()+1)>=10)? (currentDate.getMonth()+1) : '0' + (currentDate.getMonth()+1);  
-      var twoDigitDate=((currentDate.getDate())>=10)? (currentDate.getDate()) : '0' + (currentDate.getDate());
-      var date = twoDigitDate + "/" + twoDigitMonth + "/" + currentDate.getFullYear(); 
-      
+      var date_ready = formatDate(currentDate);
+
       //Update data in current table
       $('.' + name_substring).children('td').eq(1).text('$' + price);
-      $('.' + name_substring).children('td').eq(2).text(date + " " + (item.LastTradeWithTime).split(' -')[0]);
+      $('.' + name_substring).children('td').eq(2).text(date_ready + " " + (item.LastTradeWithTime).split(' -')[0]);
       
       //Determine if data has changed state or not and add highlighting if necessary    
       if (changedData[item.Name].toString() == 'true') {
@@ -44,8 +52,11 @@ checkCurrentData = function() {
     }
 
     //Update the last updated at header
-    var date = new Date();
-    $('#hd-updated').text('Last Updated at ' + date.timeNow());
+    var page_date_unformatted = new Date()
+    var page_date_formatted = formatDate(page_date_unformatted)
+    var page_time_formatted = formatTime(page_date_unformatted)
+    $('#hd-updated').text('Last Updated at ' + page_date_formatted + ' ' + page_time_formatted);
+
   })
 };
 
