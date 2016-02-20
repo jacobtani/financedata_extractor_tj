c8a8c5474900809c605d367a8de7478fd9092516
Tania Jacob <tanzjacob@gmail.com>
2016-02-20 15:46:04 +1300

    Start adding foundation for tests

---
+++ test/controllers/items_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class ItemsControllerTest < ActionController::TestCase
+
+  describe "Items Controller Tests" do
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  end
+end
---
+++ test/interactors/items_interactor_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class ItemsInteractorTest < ActiveSupport::TestCase
+
+  describe "Items Interactor Test" do
+
+
+
+
+
+
+
+  end
+
+end
