0b0eed1bee07a4483dcca75760b9eaef7a3ef603
Tania Jacob <tanzjacob@gmail.com>
2016-02-20 13:13:56 +1300

    Add error handling and more doc

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,25 +1,29 @@
  class ItemsController < ApplicationController
 
   def retrieve_current_data
-    @quote_data = Item.current_data
+    @quote_data = Item.current_data rescue nil
+    flash[:error] = 'Unable to retrieve current data' unless @quote_data
   end
 
   def retrieve_historic_data
-    @all_historic_data = Item.get_history_data
+    @all_historic_data = Item.get_history_data rescue nil
+    flash[:error] = 'Unable to retrieve historic data' unless @all_historic_data
   end
 
  def start_capture
-    site_config = SiteConfig.active
-    site_config.update_attributes(running_start: params[:running_start], running_stop: params[:running_stop])
-    system('whenever --update-crontab') #start capture
-    redirect_to retrieve_current_data_path
-  end
+   site_config = SiteConfig.active
+   site_config.update_attributes(running_start: params[:running_start], running_stop: params[:running_stop])
+   system('whenever --update-crontab') #start capture
+   flash[:success] = 'Capture process has been successfully started'
+   redirect_to retrieve_current_data_path
+ end
 
-  def stop_capture
-    site_config = SiteConfig.active
-    site_config.update_attributes(running_start: params[:running_start], running_stop: params[:running_stop])
-    system('whenever -c') #stop capture
-    redirect_to retrieve_current_data_path
-  end
+ def stop_capture
+   site_config = SiteConfig.active
+   site_config.update_attributes(running_start: params[:running_start], running_stop: params[:running_stop])
+   system('whenever -c') #stop capture
+   flash[:success] = 'Capture process has been successfully stopped'
+   redirect_to retrieve_current_data_path
+ end
 
 end
