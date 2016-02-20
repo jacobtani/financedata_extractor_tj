e00904aa664a360ccbb76ed0f2188e5d3e90a8fd
Tania Jacob <tanzjacob@gmail.com>
2016-02-20 15:46:04 +1300

    Start adding foundation for tests

---
+++ test/models/item_test.rb
@@ -0,0 +1,25 @@
+# == Schema Information
+#
+# Table name: items
+#
+#  id                            :integer          not null, primary key
+#  name                          :string(255)
+#  created_at                    :datetime
+#  updated_at                    :datetime
+
+require 'test_helper'
+
+class ItemTest < ActiveSupport::TestCase
+
+  describe "Location" do
+
+
+
+
+
+
+
+
+  end
+
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,27 @@
-ENV['RAILS_ENV'] ||= 'test'
+ENV["RAILS_ENV"] ||= "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require "minitest/autorun"
+require "minitest/spec"
+require "minitest/reporters"
+require "minitest/rails/capybara"
+require 'test_helpers/custom_matchers.rb'
+
+if ENV['SQL'] then ActiveRecord::Base.logger = Logger.new(STDOUT) if defined? ActiveRecord::Base end
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+  ActiveRecord::Migration.check_pending!
+  include Capybara::DSL
+  include Capybara::Assertions
 
-  # Add more helper methods to be used by all tests here...
+  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  #
+  # Note: You'll currently still have to declare fixtures explicitly in integration tests
+  # -- they do not yet inherit this setting
+  fixtures :all
+  Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+  def json_response
+    @json_response ||= JSON.parse(response.body)
+  end
 end
