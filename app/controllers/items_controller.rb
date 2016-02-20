5f1ea56647c9e59f9bc7b2abe0f085f24b005442
Tania Jacob <tanzjacob@gmail.com>
2016-02-20 14:31:46 +1300

    Use rails internalisation to build messages

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,19 +2,19 @@
 
   def retrieve_current_data
     @quote_data = Item.current_data rescue nil
-    flash[:error] = 'Unable to retrieve current data' unless @quote_data
+    flash[:error] = I18n.t("no_current_data") unless @quote_data
   end
 
   def retrieve_historic_data
     @all_historic_data = Item.get_history_data rescue nil
-    flash[:error] = 'Unable to retrieve historic data' unless @all_historic_data
+    flash[:error] = I18n.t("no_historic_data") unless @all_historic_data
   end
 
  def start_capture
    site_config = SiteConfig.active
    site_config.update_attributes(running_start: params[:running_start], running_stop: params[:running_stop])
    system('whenever --update-crontab') #start capture
-   flash[:success] = 'Capture process has been successfully started'
+   flash[:success] = I18n.t("start_capture")
    redirect_to retrieve_current_data_path
  end
 
@@ -22,7 +22,7 @@
    site_config = SiteConfig.active
    site_config.update_attributes(running_start: params[:running_start], running_stop: params[:running_stop])
    system('whenever -c') #stop capture
-   flash[:success] = 'Capture process has been successfully stopped'
+   flash[:success] = I18n.t("stop_capture")
    redirect_to retrieve_current_data_path
  end
 
