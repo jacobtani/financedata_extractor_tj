4a3c13744c117e467fd6b05d9619f174476ae15f
Tania Jacob <tanzjacob@gmail.com>
2016-02-20 13:43:14 +1300

    Load current stock data when application is initialised

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module FinancedataExtractor
     config.active_record.raise_in_transactional_callbacks = true
     config.stock = ['SPK.NZ', 'YHOO', 'HOH16.NYM', 'RBH16.NYM', 'HGG16.CMX', 'GCG16.CMX','PAG16.NYM', 'PLH16.NYM', 'SIG16.CMX']
     config.assets.compile= true
-
+    config.after_initialize do 
+      Item.current_data
+    end
   end
 end
