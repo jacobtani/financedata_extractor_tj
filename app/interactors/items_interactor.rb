0cdff50931f5175b69facece3b7c2cdb55e9ee75
Tania Jacob <tanzjacob@gmail.com>
2016-02-22 22:53:21 +1300

    Update items logic with subscriptions

--- app/interactors/items_interactor.rb
+++ app/interactors/items_interactor.rb
@@ -2,7 +2,6 @@ class ItemsInteractor
   include Interactor
 
   def call
-    #build url to query to yahoo api
     url = URI.parse(Item.form_url)
     begin
       data = Net::HTTP.get_response(url).body #retrieve data from url
@@ -10,6 +9,7 @@ class ItemsInteractor
       puts e.message
     end
     xml_data = Hash.from_xml(data) #turn data to xml
+    data_size = (xml_data ["query"]["results"]).size if xml_data.present?
     context.quote_data = xml_data ["query"]["results"]["quote"] if xml_data.present?
   end
 
