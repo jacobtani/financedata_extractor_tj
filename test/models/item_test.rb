4206bc10e3b6f7b69b504b761fde269a9ed164bb
Tania Jacob <tanzjacob@gmail.com>
2016-02-23 23:44:04 +1300

    Add test content

--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -4,12 +4,15 @@ class ItemTest < ActiveSupport::TestCase
 
   describe "Item Model Tests" do
 
-  	describe "Valid items" do 
+    let(:item_one)     { items(:first_item)   } 
+    let(:iain)         { users(:iain) } 
 
+    describe "Valid items" do 
 
-
-
-  	end
+      it "should have a date, price, item name and symbol" do 
+        item_one.valid?.must_equal true
+      end
+    end
 
   end
 
