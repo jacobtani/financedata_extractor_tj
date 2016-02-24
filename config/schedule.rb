df441823c2ac18861ff961a347c370f51f5d4ac8
Tania Jacob <tanzjacob@gmail.com>
2016-02-24 14:16:19 +1300

    Fix schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,11 +1,11 @@
 set :output, "log/cron_log.log"
 
 #Retrieve current stock market data 
-every 1.minutes do
+every 10.minutes do
   runner "Item.current_data", :environment => "development"
 end
 
 #Create text report every hour of stock data
-every 1.minutes do
+every :hour do
   runner "Item.generate_pdf", :environment => "development"
 end
